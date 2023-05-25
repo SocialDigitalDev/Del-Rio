c223847aeb21c513685397d124ab72d884170c59
Victória Medeiros <v.medeiros@socialdigitalcommerce.com.br>
2023-05-25 11:54:38 -0300

    fix: inclusão de zendesk

--- react/components/HelmetComponent/HelmetComponent.js
+++ react/components/HelmetComponent/HelmetComponent.js
@@ -8,6 +8,7 @@ const HelmetComponent = () => {
         <meta name="google-site-verification" content="vLzE9jJgMTr43cmd9C2AsTSVI_s2ei4nW5YMRAYWOmg" />
         <meta name="google-site-verification" content="Sm9uN48zIvnTXEN1h8xbxwoVzUh2W0PbP9vJuZTT3Fw" />
         <meta data-react-helmet="true" name="robots" content="index, follow" />
+        <script id="ze-snippet" src="https://static.zdassets.com/ekr/snippet.js?key=f3cd4b59-86ca-4f09-a9cf-75a8f54b9525"></script>
       </Helmet>
     </>
   )
