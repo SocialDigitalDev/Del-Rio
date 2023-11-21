e78cf9b466a2786539c7433e0b9164df7307f8f3
Jonas Di Pierro <j.dipierro@socialdigitalcommerce.com.br>
2023-11-21 11:58:04 -0300

    feat: bloco de SEO custom para a agência

---
+++ react/components/SeoTextBlock/index.js
@@ -0,0 +1,35 @@
+import React, {useState} from "react";
+import { schema } from "./schema";
+import '../../css/SeoTextBlock/global.css';
+
+const SeoTextBlock = ({ enableComponent = false, seoTitle, seoText }) => {
+    
+    let [ seeMore, setSeeMore ] = useState('ver-mais');
+    let [ textSeeMore, setTextSeeMore] = useState('Ver Mais');
+
+    function changeSeeMoreState() {
+        if ( seeMore === "ver-mais" ){
+            setSeeMore('ver-menos')
+            setTextSeeMore('Ver Menos')
+        } else {
+            setSeeMore('ver-mais')
+            setTextSeeMore('Ver Mais')
+        }
+    }
+
+    return (
+        <>
+            {enableComponent && (
+                <div className={`seo-custom-block ${seeMore}`}>
+                    <h1 className="seo-custom-title" dangerouslySetInnerHTML={{__html: seoTitle }} />
+                    <div className={`seo-custom-text ${seeMore}`} dangerouslySetInnerHTML={{__html: seoText }} />
+                    <button className="seo-custom-see-more-less" onClick={changeSeeMoreState}>{textSeeMore}</button>
+                </div>
+            )}
+        </>
+    );
+}
+
+SeoTextBlock.schema = schema;
+
+export default SeoTextBlock;
